58fce185637f19b7824a27233717e19197e5b730
statar83 <statarusa@gmail.com>
2019-12-10 13:33:29 -0500

    hw commit

--- src/test/java/tests/NavigationTests.java
+++ src/test/java/tests/NavigationTests.java
@@ -7,10 +7,12 @@ import utilities.StringUtility;
 
 public class NavigationTests {
 
+
     public WebDriver driver;
 
     @Test(description = "verify titles (chrome)")
-    public void testChrome(){
+    public void testChrome() {
+
         driver = BrowserFactory.getDriver("chrome");//1
         driver.get("https://google.com");//2
         String googleTitle = driver.getTitle();//3
@@ -24,7 +26,22 @@ public class NavigationTests {
 
         driver.quit();
     }
-    //@Test(description = "verify titles(firefox)")
+
+    @Test(description = "verify titles (firefox)")
+    public void testfirefox() {
+
+        driver = BrowserFactory.getDriver("firefox");//1
+        driver.get("https://google.com");//2
+        String googleTitle = driver.getTitle(); //3
+        driver.get("https://etsy.com");//4
+        String etsyTitle = driver.getTitle();//5
+        driver.navigate().back();//6
+        StringUtility.verifyEquals(googleTitle, driver.getTitle());//7
+        driver.navigate().forward();//8
+        StringUtility.verifyEquals(etsyTitle, driver.getTitle());//9
 
 
+        driver.quit();
+    }
 }
+
